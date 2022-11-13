231fcf26386515277c149adf5b932b17a0e6bb09
Faby <72978296+fabricemlili@users.noreply.github.com>
2022-11-13 23:30:55 +0100

    Delete API_SearchMovieInfo directory

--- API_SearchMovieInfo/Get_info/imDb_id.py
+++
@@ -1,7 +0,0 @@
-from Get_info.imBd_result import IMDbResult
-
-
-class IMDbid:
-
-    def find_id(moviesearch):
-        return IMDbResult.get_infos(moviesearch).content['results'][0]['id']
--- API_SearchMovieInfo/Get_info/versioning_event.py
+++
@@ -1,4 +0,0 @@
-class VersioningEvent:
-    def __init__(self, status_code, content):
-        self.status_code = status_code
-        self.content = content
